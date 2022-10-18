b7d844e1e225c0d87cbd751ca127d233a31e73dc
João Vitor Brandão <joaovitorbrandao88@gmail.com>
2022-10-18 09:27:43 -0300

    projeto pronto

--- SoltaOSom/musica.py
+++ SoltaOSom/musica.py
@@ -1,6 +1,7 @@
 import pygame
 
 
+
 def dar_play_em_cond():
     pygame.init()
     pygame.mixer.music.load("conderosa.mp3")
@@ -12,3 +13,12 @@ def dar_play_em_bardo():
     pygame.mixer.music.load("oasisbardo.mp3")
     pygame.mixer.music.play()
     pygame.event.wait()
+
+def dar_play_em_sweat():
+    pygame.init()
+    pygame.mixer.music.load("sweater.mp3")
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+
+
--- SoltaOSom/tela.py
+++ SoltaOSom/tela.py
@@ -3,13 +3,24 @@ from tkinter import *
 
 janela = Tk()
 janela.title("Tocador de musicas com Pygame")
-janela.geometry("400x400")
+janela.geometry("300x300")
 
 texto_orientacao = Label(janela,text="Clique em um dos botões para tocar alguma musica.",padx= 20,pady= 20)
 texto_orientacao.grid(column=0,row=0)
 
-musica1 = Button(janela, text="Saudades da Rosa", command=dar_play_em_cond,padx=0,pady= 10)
-musica1.grid(column=0,row=2)
 
-musica2 = Button(janela, text="Oasis - Bardo", command=dar_play_em_bardo,padx= 12,pady= 10)
-musica2.grid(column=1,row=2)
+musica1 = Button(janela, text="Saudades da Rosa", command = dar_play_em_cond,padx=0,pady= 10)
+musica1.grid(column=0 , row=1)
+
+musica2 = Button(janela, text="Oasis - Bardo", command = dar_play_em_bardo,padx= 12,pady= 10)
+musica2.grid(column=0,row=2)
+
+musica3 = Button(janela, text="Sweater weather", command = dar_play_em_sweat,padx= 5,pady= 12)
+musica3.grid(column=0,row=3)
+
+
+
+texto_fim = Label(janela,text="")
+texto_fim.grid(column=0,row=8,padx=0,pady=20)
+
+janela.mainloop()
