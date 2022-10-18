30de3c8cf88667998337ff2a4bd6a7d10867d97b
João Vitor Brandão <joaovitorbrandao88@gmail.com>
2022-10-18 09:57:18 -0300

    Fim!

--- SoltaOSom/tela.py
+++ SoltaOSom/tela.py
@@ -1,26 +1,27 @@
 from musica import *
 from tkinter import *
 
-janela = Tk()
-janela.title("Tocador de musicas com Pygame")
-janela.geometry("300x300")
+def telaum():
+    janela = Tk()
+    janela.title("Tocador de musicas com Pygame")
+    janela.geometry("300x300")
 
-texto_orientacao = Label(janela,text="Clique em um dos botões para tocar alguma musica.",padx= 20,pady= 20)
-texto_orientacao.grid(column=0,row=0)
-
-
-musica1 = Button(janela, text="Saudades da Rosa", command = dar_play_em_cond,padx=0,pady= 10)
-musica1.grid(column=0 , row=1)
-
-musica2 = Button(janela, text="Oasis - Bardo", command = dar_play_em_bardo,padx= 12,pady= 10)
-musica2.grid(column=0,row=2)
-
-musica3 = Button(janela, text="Sweater weather", command = dar_play_em_sweat,padx= 5,pady= 12)
-musica3.grid(column=0,row=3)
+    texto_orientacao = Label(janela,text="Clique em um dos botões para tocar alguma musica.",padx= 20,pady= 20)
+    texto_orientacao.grid(column=0,row=0)
 
 
+    musica1 = Button(janela, text="Saudades da Rosa", command = dar_play_em_cond,padx=0,pady= 10)
+    musica1.grid(column=0 , row=1)
 
-texto_fim = Label(janela,text="")
-texto_fim.grid(column=0,row=8,padx=0,pady=20)
+    musica2 = Button(janela, text="Oasis - Bardo", command = dar_play_em_bardo,padx= 12,pady= 10)
+    musica2.grid(column=0,row=2)
 
-janela.mainloop()
+    musica3 = Button(janela, text="Sweater weather", command = dar_play_em_sweat,padx= 5,pady= 12)
+    musica3.grid(column=0,row=3)
+
+
+
+    texto_fim = Label(janela,text="")
+    texto_fim.grid(column=0,row=8,padx=0,pady=20)
+
+    janela.mainloop()
